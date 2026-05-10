f68ca5ed7feccfea431e88c51b7ebc203dc2d39b
GaeulOh <ogy1004@naver.com>
2026-05-10 22:32:59 +0900

    Move Zeroes

---
+++ Top_Interview/Array/Move Zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int>& nums)
+    {
+        int zeroIndex = 0;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != 0)
+            {
+                if(i != zeroIndex)
+                {
+                    swap(nums[zeroIndex], nums[i]);
+                }
+
+                zeroIndex++;
+            }
+        }
+    }
+};
